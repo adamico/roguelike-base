f8b4eace59acb4527da879ad6dc3ca81ba075107
Andrea D'Amico <andrea.damico@gmail.com>
2025-03-24 14:24:02 +0100

    tileset lowercase name

--- mygame/app/tileset.rb
+++ mygame/app/tileset.rb
@@ -3,7 +3,7 @@ TILES = {
 }.freeze
 
 def build_tileset
-  tileset = CP437SpritesheetTileset.new(path: 'sprites/Zilk-16x16.png', w: 256, h: 256)
+  tileset = CP437SpritesheetTileset.new(path: 'sprites/zilk-16x16.png', w: 256, h: 256)
   TILES.each do |name, attributes|
     tileset.define_tile name, attributes
   end
